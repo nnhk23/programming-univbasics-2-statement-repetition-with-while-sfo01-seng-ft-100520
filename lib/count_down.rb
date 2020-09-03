4c3fec6f3841f10430050110eb5e3b3180a4beda
Kim Nguyen <nnhk23@gmail.com>
2020-09-03 23:06:14 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,8 @@
 # Write your code here
 count = 10 
 smol = 1 
-while count > 0 do break if count == smol 
+while count > 0 do 
+  break if count == smol 
   puts "Happy New Year!"
   puts count
   count -= 1 
