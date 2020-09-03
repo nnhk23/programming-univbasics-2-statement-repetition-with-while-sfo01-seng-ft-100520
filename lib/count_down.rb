f952eadccd953e4c2c472ce43262f520c1bc3abf
Kim Nguyen <nnhk23@gmail.com>
2020-09-03 23:08:12 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,13 +1,7 @@
 # Write your code here
-# count = 10 
-# while count > 0 do 
-#   puts count
-#   count -= 1 
-# end
-# puts "Happy New Year!"
-magic_exit_number = 7
-count = 0
-while count < 10 && count != magic_exit_number do
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
+count = 10 
+while count > 0 do 
+  puts count
+  count -= 1 
 end
+puts "Happy New Year!"
